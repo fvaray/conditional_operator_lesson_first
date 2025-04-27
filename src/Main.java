c93e171926304cc4e0da1593611fdd1de0589190
fvaray <fvaray@gmail.com>
2025-04-27 21:15:21 +0700

    задача 1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.println("Задача 1");
+        System.out.println(" ");
+        System.out.println("Задача 1");
+        byte Age = 1;
+        if (Age >= 18 ){
+        System.out.println("он совершеннолетний");}
+        if (Age <= 0){
+            System.out.println("либо младенец, либо еще не родился");        }
+        else
+        {
+            System.out.println("он не достиг совершеннолетия, нужно немного подождать");
+        }
 
     }
 }
